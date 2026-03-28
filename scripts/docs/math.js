b39c9fffc522da55ac1fc39b6724f061065e2b0f
mintCreeper <92992499+mintCreeper@users.noreply.github.com>
2026-03-29 00:21:17 +0900

    add math lib

---
+++ scripts/docs/math.js
@@ -0,0 +1,52 @@
+/**
+ * 指定した小数で四捨五入した値を返します
+ * @param { number } number 四捨五入する値
+ * @param { number } digit 小数第何位を四捨五入するのか
+ * @returns { number }
+ */
+
+export function setRound(number, digit) {
+    try {
+        const keta = digit - 1
+        const suuji = number * 10 ** keta
+        const kaketaAtai = Math.round(suuji)
+        return kaketaAtai / 10 ** keta
+    } catch (error) {
+        console.error(error) }
+}
+
+
+/**
+ * 配列の中の一番大きな数字のインデックスを返します
+ * @argument {number[]} array 取得する配列
+ * @returns {number}
+ */
+
+export function getMaxIndex(array) {
+    try {
+        const index = array.findIndex(Math.max(...array))
+        return index
+    } catch (error) {
+        console.error(error) }
+}
+
+
+/**
+ * 2点の値の中間値を返します
+ * @argument {number} number1 比較する値その1
+ * @argument {number} number2 比較する値その2
+ * @returns {number}
+ */
+
+export function middleNumber(number1, number2) {
+    try {
+        if (number1 > number2) {
+            const middlePoint = Math.round((number1 - number2) / 2)
+            return number2 + middlePoint
+        } else {
+            const middlePoint = Math.round((number2 - number1) / 2)
+            return number1 + middlePoint
+        }
+    } catch (error) {
+        console.error(error) }
+}
